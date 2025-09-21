6df96abb8d521b43625dc0c49ba480edbf47e76a
Adele <2204265@uad.ac.uk>
2025-09-21 19:14:23 +0100

    Started work on making cards
    temporary gif

--- assets/js/gradient.js
+++ assets/js/gradient.js
@@ -1,4 +1,4 @@
   let i = 0;
-  setInterval(() => { document.documentElement.style.setProperty('--color', `hsl(${i}, 100%, 50%)`);
-    i = (i + 1) % 360;
-  }, 100); // Change every 100ms (adjust speed as needed)
+  setInterval(() => { document.documentElement.style.setProperty('--color', `hsl(${i/2}, 100%, 50%)`);
+    i = (i + 1) % 720;
+  }, 50); // Change every 100ms (adjust speed as needed)
