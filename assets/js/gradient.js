3f85265207cbecc13c18d8d1fcafb47146c82c26
Adele <09sabau@gmail.com>
2026-09-23 02:52:47 +0100

    Better Gradient

--- assets/js/gradient.js
+++ assets/js/gradient.js
@@ -1,4 +1,6 @@
   let i = 0;
-  setInterval(() => { document.documentElement.style.setProperty('--color', `hsl(${i/2}, 100%, 50%)`);
+  setInterval(() => { 
+    document.documentElement.style.setProperty('--color', `hsl(${i/2}, 100%, 50%)`);
+    document.documentElement.style.setProperty('--secondaryColor', `hsl(${i/2 + 50}, 100%, 50%)`);
     i = (i + 1) % 720;
   }, 50); // Change every 100ms (adjust speed as needed)
